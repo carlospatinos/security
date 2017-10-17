7233ade4df85b1ffd8458ae177b0ab26ecdf251d
Carlos Patino <carlospatinos@gmail.com>
2017-10-17 19:21:38 +0100

    adding the database

---
+++ assignment_hash/saltHashCracker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+import sqlite3 as lite
+import sys
+
+con = None
+
+try:
+    con = lite.connect('garda.sqlite')
+    
+    cur = con.cursor()    
+    cur.execute('SELECT SQLITE_VERSION()')
+    
+    data = cur.fetchone()
+    
+    print "SQLite version: %s" % data                
+    
+except lite.Error, e:
+    
+    print "Error %s:" % e.args[0]
+    sys.exit(1)
+    
+finally:
+    
+    if con:
+        con.close()
