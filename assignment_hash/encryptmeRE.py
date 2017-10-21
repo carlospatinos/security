d2e4b3eb951dcfa2d336155aa4b1fe41df92d78c
Carlos Patino <carlospatinos@gmail.com>
2017-10-21 15:57:02 +0100

    Decoding the bad encryption

--- assignment_hash/encryptmeRE.py
+++ assignment_hash/encryptmeRE.py
@@ -1,4 +1,5 @@
-# This is the program we believe was used to encode the intercepted message.
+#!/usr/bin/python 
+# # This is the program we believe was used to encode the intercepted message.
 # some of the retrieved program was damaged (show as &&&&)
 # Can you use this to figure out how it was encoded and decode it? 
 # Good Luck
@@ -6,6 +7,7 @@
 import string
 import random
 import unittest
+import sys, traceback
 from base64 import b64encode, b64decode
 from itertools import permutations
 
@@ -15,6 +17,7 @@ from itertools import permutations
 # of the reverse part of the encoding to try to decode
 
 secret = '&&&&&&&&&&&&&&' # We don't know the original message or length
+iterations=0
 
 def step1(s):
 	_step1 = string.maketrans("zyxwvutsrqponZYXWVUTSRQPONmlkjihgfedcbaMLKJIHGFEDCBA",
@@ -28,7 +31,13 @@ def step1r(s):
 
 def step2(s): return b64encode(s)
 
-def step2r(s): return b64decode(s)
+def step2r(s):
+	value = ""
+	try:
+		value = b64decode(s)
+	except:
+		print "Error decoding string"
+	return  value
 
 def step3(plaintext, shift=4):
     loweralpha = string.ascii_lowercase
@@ -55,19 +64,34 @@ def make_secret(plain, count):
 	return a
 
 def recover_secret():
-	lines = [line.rstrip('\n') for line in open('intercepted.txt')]
+	lines = [line.rstrip('\n') for line in open('intercepted7.txt')]
 	try:
 		for l in lines:
-			iterations = int(l[0])
-			print "Number of iterations are: {}".format(iterations)
-			for count in xrange(iterations):
-				for al in super_dencoding:
-    					print "function: {} => {}".format(al.__name__, al(l))
+			userAlgorithm = int(l[0])
+			lineToDecode = l[1:]
+			print "User Algorithm: {}".format(super_dencoding[userAlgorithm-1].__name__)
+			#for count in xrange(userAlgorithm):
+			#for algorithm in super_dencoding:
+			print super_dencoding[userAlgorithm-1](lineToDecode)
 	except ValueError:
 		print "Error in number"
 
-	#print '2{}'.format(b64encode(plaintext))
-	
+def recover_secret_2(algorithmNumber, message):
+	global iterations 
+	iterations = iterations+1
+	keyToQuit = raw_input('Try again? ')
+	if keyToQuit.lower() == "q":
+		sys.exit(0)
+	decodedMessage = super_dencoding[algorithmNumber-1](message)
+	print "message: [{}]".format(decodedMessage)
+	print "Iteration [{}]. Used Algorithm: {}".format(iterations, super_dencoding[algorithmNumber-1].__name__)
+	try:
+		newIndexAlgorithm = int(decodedMessage[0])
+		newMessage = decodedMessage[1:]
+		recover_secret_2(newIndexAlgorithm, newMessage)
+	except ValueError:
+		print "The index is not an int anymore. Message should be clear now"
+		sys.exit(0)
 
 class MyTest(unittest.TestCase):
     def test_step1(self):
@@ -82,13 +106,23 @@ class MyTest(unittest.TestCase):
         s = 'Hola companeros'
         self.assertEqual(s, step3r(step3(s)))
 
-
-
+def main():
+	try:
+		print "Please press q to exit."
+		lines = [line.rstrip('\n') for line in open('intercepted.txt')]
+		for l in lines:
+			algorithmIndex = int(l[0])
+			messageToDecode = l[1:]
+			recover_secret_2(algorithmIndex, messageToDecode)
+	except KeyboardInterrupt:
+		print "Shutdown requested...exiting"
+	except Exception:
+		traceback.print_exc(file=sys.stdout)
+	sys.exit(0)
 
 if __name__ == '__main__':
 	#unittest.main()
+	main()
 	#print make_secret(secret, count=&&&)
-	#313312
-	recover_secret()
     	
 
